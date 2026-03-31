de4b598b3bc0cbcaa5876a35b981b29b9200c50a
jose guzman <joseguzman1337@users.noreply.github.com>
2026-03-31 00:51:27 -0500

    [CRUSH] chore(repo): checkpoint pending changes #TASK-000

--- lib/hamlbars/version.rb
+++
@@ -1,3 +0,0 @@
-module Hamlbars
-  VERSION = '2.1.1'
-end
--- spec/sprockets_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe "Sprockets" do
-  it "should register .hamlbars extension" do
-    expect(Sprockets.engines('.hamlbars')).to be(Hamlbars::Template)
-  end
-end
